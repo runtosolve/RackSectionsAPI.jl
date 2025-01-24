8e379f7db6ae949620851a7eee8b5c107b8ba4c0
Cris Moen <cris.moen@runtosolve.com>
2025-01-24 10:11:04 -0500

    comment out Show calls

--- src/RackSectionsAPI.jl
+++ src/RackSectionsAPI.jl
@@ -90,112 +90,112 @@ function perform_calculation(event_data)
     if input.section_type == "step_beam"
       section_details = StructTypes.constructfrom(RackSections.Beams.StepBeamInput, input.section_details)
       properties = RackSections.Beams.step_beam(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.step_beam(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.step_beam(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "angled_step_beam"
       # section_input = JSON3.read(input.section_details, RackSections.Beams.AngledStepBeamInput)
       section_details = StructTypes.constructfrom(RackSections.Beams.AngledStepBeamInput, input.section_details)
       # section_details = StructTypes.constructfrom(RackSections.Beams.StepBeamInput, section_input[:section_details])
       properties = RackSections.Beams.angled_step_beam(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.step_beam(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.step_beam(properties, input.api_figure_options)
+      # end
     end
   elseif input.member_type == "column"
     if input.section_type == "closed_tube"
       section_details = StructTypes.constructfrom(RackSections.Columns.RectangularTubeInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.RectangularTubeInput)
       properties = RackSections.Columns.rectangular_tube(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.closed_tube_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.closed_tube_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "cee_with_lips"
       section_details = StructTypes.constructfrom(RackSections.Columns.CeeLipsInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.CeeLipsInput)
       properties = RackSections.Columns.cee_with_lips(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "cee_with_lips_and_rib"
       section_details = StructTypes.constructfrom(RackSections.Columns.CeeLipsRibInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.CeeLipsRibInput)
       properties = RackSections.Columns.cee_with_lips_rib(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "hat_with_rib"
       section_details = StructTypes.constructfrom(RackSections.Columns.HatRibInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.HatRibInput)
       properties = RackSections.Columns.hat_with_rib(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "hat_with_lips_and_rib"
       section_details = StructTypes.constructfrom(RackSections.Columns.HatLipsRibInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.HatLipsRibInput)
       properties = RackSections.Columns.hat_with_lips_rib(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "hat_with_lips_and_trapezoidal_rib"
       section_details = StructTypes.constructfrom(RackSections.Columns.HatLipsTrapezoidalRibInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.HatLipsTrapezoidalRibInput)
       properties = RackSections.Columns.hat_with_lips_trapezoidal_rib(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.hat_with_rib_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "unistrut_with_lips_in"
       section_details = StructTypes.constructfrom(RackSections.Columns.UniStrutInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.UniStrutInput)
       properties = RackSections.Columns.unistrut_in(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.unistrut_in_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.unistrut_in_column(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "unistrut_with_lips_out"
       section_details = StructTypes.constructfrom(RackSections.Columns.UniStrutInput, input.section_details)
       # section_input = JSON3.read(input.section_details, RackSections.Columns.UniStrutInput)
       properties = RackSections.Columns.unistrut_out(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.unistrut_out_column(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.unistrut_out_column(properties, input.api_figure_options)
+      # end
     end
   elseif input.member_type == "brace"
     if input.section_type == "cee_with_lips"  
       # section_input = JSON3.read(input.section_details, RackSections.Braces.CeeLipsBraceInput)
       section_details = StructTypes.constructfrom(RackSections.Braces.CeeLipsBraceInput, input.section_details)
       properties = RackSections.Braces.cee_with_lips_brace(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_brace(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.cee_with_lips_brace(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "cee"
       # section_input = JSON3.read(input.section_details, RackSections.Braces.CeeInput)
       section_details = StructTypes.constructfrom(RackSections.Braces.CeeInput, input.section_details)
       properties = RackSections.Braces.cee(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.cee_brace(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.cee_brace(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "pipe"
       # section_input = JSON3.read(input.section_details, RackSections.Braces.PipeInput)
       section_details = StructTypes.constructfrom(RackSections.Braces.PipeInput, input.section_details)
       properties = RackSections.Braces.pipe(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.pipe_brace(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.pipe_brace(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "formed_angle"
       # section_input = JSON3.read(input.section_details, RackSections.Braces.AngleInput)
       section_details = StructTypes.constructfrom(RackSections.Braces.AngleInput, input.section_details)
       properties = RackSections.Braces.angle(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.angle_brace(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.angle_brace(properties, input.api_figure_options)
+      # end
     elseif input.section_type == "closed_tube"
       # section_input = JSON3.read(input.section_details, RackSections.Braces.RectangularTubeBraceInput)
       section_details = StructTypes.constructfrom(RackSections.Braces.RectangularTubeBraceInput, input.section_details)
       properties = RackSections.Braces.rectangular_tube_brace(section_details)
-      if input.create_CUFSM_figure_files == true
-        all_figures_IO, all_figures, figure_labels = Show.rectangular_tube_brace(properties, input.api_figure_options)
-      end
+      # if input.create_CUFSM_figure_files == true
+      #   all_figures_IO, all_figures, figure_labels = Show.rectangular_tube_brace(properties, input.api_figure_options)
+      # end
     end
   end
 
